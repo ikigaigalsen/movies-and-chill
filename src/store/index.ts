814f5b322e8b512aba83424f13c44b53185b9ca9
ikigaigalsen <jaayahmad@gmail.com>
2020-02-05 20:56:51 +0800

    Added comments for Rematch

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,6 +2,12 @@ import { init, RematchRootState } from "@rematch/core";
 import { movies } from "./movies";
 
 const models = { movies };
+
+/**
+ * Since we are using Rematch (A Redux Framework), we don't need to
+ * initailize reducers, dispatcher, etc., Rematch simplifies it all for us
+ * and makes Redux a breeze to use
+ */
 export const store = init({
   models
 });
