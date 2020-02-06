5e7c0a0cfcc49701fae0c278d2c9bc79493a4217
ikigaigalsen <jaayahmad@gmail.com>
2020-02-06 17:11:04 +0800

    Added stylings and release year and ratings for movie tile

--- src/components/MovieWidget/index.tsx
+++ src/components/MovieWidget/index.tsx
@@ -12,7 +12,9 @@ interface IMovieWidgetProps {
 const MovieWidget: React.FC<IMovieWidgetProps> = ({ movie }) => {
   return (
     <div className={styles.movieBox}>
-      <div className={styles.movieText}>{movie.title}</div>
+      <div className={styles.movieTitle}>{movie.title}</div>
+      <div className={styles.movieYear}>{movie.release_date.split('-')[0]}</div>
+      <div className={styles.movieRating}>{movie.vote_average}</div>
       <img
         className={styles.poster}
         src={`${IMAGE_URL}/w500/${movie.poster_path}`}
