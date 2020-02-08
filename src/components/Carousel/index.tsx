3b16064a3dc44abac4f5255aa197ce71f0d071cf
ikigaigalsen <jaayahmad@gmail.com>
2020-02-08 19:45:34 +0800

    Carousel Completed

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -4,6 +4,7 @@ import { IMoviesList } from "../../store/types";
 import { IMAGE_URL } from "../../constants/api";
 
 import styles from "./index.module.scss";
+import { Link } from "react-router-dom";
 
 interface ICarouselProps {
   treadingMovies: IMoviesList;
@@ -14,11 +15,22 @@ const Carousel: React.FC<ICarouselProps> = ({ treadingMovies }) => {
     <ReactCarousel autoPlay={5000} dots>
       {treadingMovies.results?.slice(0, 5).map(movie => (
         <div key={movie.id} className={styles.root}>
-          <img
-            src={`${IMAGE_URL}/w1280/${movie.backdrop_path}`}
-            alt={movie.title}
-            className={styles.image}
-          />
+          <div className={styles.imageWrap}>
+            <img
+              src={`${IMAGE_URL}/w1280/${movie.backdrop_path}`}
+              alt={movie.title}
+              className={styles.image}
+            />
+          </div>
+          <Link to={`/movies/${movie.id}`}>
+            <div className={styles.text}>
+              <div className={styles.movieTitle}>
+                {movie.title} ({movie.release_date.split("-")[0]})
+              </div>
+              <div className={styles.rating}>Rating: {movie.vote_average}</div>
+              <div>{movie.overview}</div>
+            </div>
+          </Link>
         </div>
       ))}
     </ReactCarousel>
